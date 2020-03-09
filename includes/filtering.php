1089fd7d979024c86052224bd85aa1dcfd060eaf
Kovalchik8 <37037738+Kovalchik8@users.noreply.github.com>
2020-03-09 22:35:34 +0200

    New php filtering code

--- includes/filtering.php
+++ includes/filtering.php
@@ -9,58 +9,35 @@ function get_filtered_items() {
   $result = array( 'products' => array() );
   
   // initial args
-  $args = array(
-    'post_type' => 'products',
-    'posts_per_page' => -1
-  );
-  
+  $args = array('post_type' => 'products', 'posts_per_page' => -1);
   $tax_query = array('relation' => 'AND');
   $meta_query = array('relation' => 'AND');
 
-  // set filter by taxonomy class
-  $tax_query[] = array(
-    array (
-      'taxonomy' => 'class',
-      'field'    => 'id',
-      'terms'    => esc_sql($_POST['class']),
-    ));
-
-  // set filter by taxonomy category
-  if ($_POST['category']) {
-    $tax_cat[] = array(
-      array (
-        'taxonomy' => 'category',
-        'field'    => 'term_id',
-        'terms'    => esc_sql( $_POST['category'] ),
-      ));
-    $tax_query[] = $tax_cat;
-  }
-
-  // set filter by taxonomy color
-  if ($_POST['color']) {
-    $tax_color[] = array(
-      array (
-        'taxonomy' => 'color',
-        'field'    => 'term_id',
-        'terms'    => esc_sql( $_POST['color'] ),
-      ));
-    $tax_query[] = $tax_color;
-  }
+  // set query by taxonomies
+  $taxonomies = ['class', 'category', 'color'];
+  foreach($taxonomies as $tax)
+    if (!empty($_POST[$tax])) {
+      $tax_query_args[] = array(
+        'taxonomy' => $tax,
+        'field'    => 'id',
+        'terms'  => $_POST[$tax]
+      );
+      $tax_query[] = $tax_query_args;
+    }
   
-  $args['tax_query'] = $tax_query;
+  $args['tax_query'] = $tax_query; 
 
-  // set filter by meta fields
-  $meta_params = ['size', 'physical', 'watts', 'cct', 'cri'];
-  foreach($meta_params as $meta) {
+  // set query by meta fields
+  $meta_fields = ['size', 'physical', 'watts', 'cct', 'cri'];
+  foreach($meta_fields as $meta)
     if ( !empty($_POST[$meta])) {
-      $meta_query_param[] = array(
+      $meta_query_args[] = array(
         'key'     => 'product_params_' . $meta,
         'value'   => esc_sql( $_POST[$meta] ),
         'compare' => 'IN',
       );
-      $meta_query[] = $meta_query_param;
+      $meta_query[] = $meta_query_args;
     }
-  }
   
   $args['meta_query'] = $meta_query;
 
@@ -70,14 +47,12 @@ function get_filtered_items() {
   while($products->have_posts()) {
     $products->the_post();
 
-    if (get_post_type() == 'products') {
+    if (get_post_type() == 'products')
       array_push($result['products'], array(
         'title' => get_the_title(),
         'link' => get_the_permalink(),
         'image' => get_field('product_gallery')[0],
       ));
-    }
-    
   }
 
   echo json_encode($result);
